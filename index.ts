00b8268c2dede985182a5b2fec5f8c28e18399f2
Neo Ryo <marco.sch4064@gmail.com>
2024-06-14 16:16:45 +0200

    update + jest

--- index.ts
+++ index.ts
@@ -1,20 +1,18 @@
-import express from "express";
-import cors from "cors";
-import morgan from "morgan";
-import bodyParser from "body-parser";
+import express from 'express'
+import cors from 'cors'
+import morgan from 'morgan'
+import bodyParser from 'body-parser'
 
-const app = express();
-app.use(cors());
-app.use(express.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(
-    morgan(":method :url :status :res[content-length] - :response-time ms")
-);
+const app = express()
+app.use(cors())
+app.use(express.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
 
-app.get("/", (_req, res) => {
-    res.status(200).json("Welcome!");
-});
+app.get('/', (_req, res) => {
+    res.status(200).json('Welcome!')
+})
 
 app.listen(9000, () => {
-    console.log("Listenin on PORT 9000");
-});
+    console.log('Listenin on PORT 9000')
+})
---
+++ tests/index.test.ts
@@ -0,0 +1,7 @@
+import { expect, test, describe } from '@jest/globals'
+
+describe('suite 1', () => {
+    test('adds 1 + 2 to equal 3', () => {
+        expect(1 + 2).toBe(3)
+    })
+})
